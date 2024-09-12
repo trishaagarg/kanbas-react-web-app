8742ee4a36414317b9862486df64e3ef56e0f3f1
trishaagarg <garg.trishaaa@gmail.com>
2024-09-12 16:14:37 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Labs from './Labs';
+
+function App() {
+  return (
+    <div>
+    <h1> Welcome to Web dev</h1>
+    <Labs /> 
+    </div>
+  );
+}
+
+export default App;
