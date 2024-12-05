1906c8a6050241ffdaaae9fce9c4fb69f4f2dcd5
trishaagarg <garg.trishaaa@gmail.com>
2024-12-05 11:37:18 -0500

    a6

--- src/Kanbas/Courses/People/Tables.tsx
+++ src/Kanbas/Courses/People/Tables.tsx
@@ -1,49 +1,49 @@
+import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "./Details";
+import * as client from "../../Account/client";
+import { Link } from "react-router-dom";
 
-export default function PeopleTable() {
+
+export default function PeopleTable({ users = [] }: { users?: any[] }) {
   return (
     <div id="wd-people-table">
+            <PeopleDetails />
       <table className="table table-striped">
         <thead>
-          <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
         </thead>
         <tbody>
-          <tr><td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Tony</span>{" "}
-              <span className="wd-last-name">Stark</span></td>
-            <td className="wd-login-id">001234561S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-10-01</td>
-            <td className="wd-total-activity">10:21:32</td> </tr>
-          <tr><td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Barry</span>{" "}
-              <span className="wd-last-name">Allen</span></td>
-            <td className="wd-login-id">001234562S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-09-11</td>
-            <td className="wd-total-activity">11:11:42</td> </tr>
-          <tr><td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Victor</span>{" "}
-              <span className="wd-last-name">Von Doom</span></td>
-            <td className="wd-login-id">000000001I</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">Instructor</td>
-            <td className="wd-last-activity">2024-10-08</td>
-            <td className="wd-total-activity">99:99:99</td> </tr>
-          <tr><td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Reed</span>{" "}
-              <span className="wd-last-name">Richards</span></td>
-            <td className="wd-login-id">001234564S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">TA</td>
-            <td className="wd-last-activity">2023-15-25</td>
-            <td className="wd-total-activity">11:28:02</td> </tr>
+        {users
+        .map((user) => (
+          <tr key={user._id}>
+            <td className="wd-full-name text-nowrap">
+              <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
+                <FaUserCircle className="me-2 fs-1 text-secondary" />
+                <span className="wd-first-name">{user.firstName}</span>{" "}
+                <span className="wd-last-name">{user.lastName}</span>
+              </Link>
+            </td>
+              <td className="wd-login-id">{user.loginId || "N/A"}</td>
+              <td className="wd-section">{user.section || "N/A"}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">
+                {user.lastActivity || "Not Available"}
+              </td>
+              <td className="wd-total-activity">
+                {user.totalActivity || "Not Available"}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-    </div> );}
+    </div>
+  );
+}
